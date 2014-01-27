694517603ccac0a68f815f32f57e12210f7f6a77
Leila Lp <ly4ster@gmail.com>
2014-01-26 20:26:43 -0500

    details event activity added

--- VAO/gen/com/cofradia/vao/R.java
+++ VAO/gen/com/cofradia/vao/R.java
@@ -24,11 +24,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int button1=0x7f080003;
-        public static final int editText1=0x7f080001;
-        public static final int imageView1=0x7f080000;
-        public static final int textView1=0x7f080002;
+        public static final int action_settings=0x7f080005;
+        public static final int button1=0x7f080004;
+        public static final int imgEvent=0x7f080000;
+        public static final int swtEvent=0x7f080003;
+        public static final int txtEventDescription=0x7f080002;
+        public static final int txtEventTitle=0x7f080001;
     }
     public static final class layout {
         public static final int activity_event_details=0x7f030000;
@@ -39,10 +40,15 @@ public final class R {
         public static final int login=0x7f070001;
     }
     public static final class string {
+        public static final int EventAttendance=0x7f05000a;
+        public static final int EventDescription=0x7f050009;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int btnLogin=0x7f050005;
+        public static final int eventDescription=0x7f050008;
+        public static final int eventTitle=0x7f050007;
         public static final int hello_world=0x7f050002;
+        public static final int imageEvent=0x7f050006;
         public static final int picture_event=0x7f050004;
         public static final int title_activity_event_details=0x7f050003;
     }
