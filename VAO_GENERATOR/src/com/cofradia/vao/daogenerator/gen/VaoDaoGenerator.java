93dd1978cbc0903fb07a428a2b8d66e6c7c05e3b
Katherine <ktucto@gmail.com>
2014-01-29 23:38:11 -0500

    adding user

--- VAO_GENERATOR/src/com/cofradia/vao/daogenerator/gen/VaoDaoGenerator.java
+++ VAO_GENERATOR/src/com/cofradia/vao/daogenerator/gen/VaoDaoGenerator.java
@@ -1,16 +1,24 @@
 package com.cofradia.vao.daogenerator.gen;
 
 import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
 import de.greenrobot.daogenerator.Schema;
 
 public class VaoDaoGenerator {
 	public static void main(String[] args) throws Exception {
         Schema schema = new Schema(1, "de.greenrobot.daovao");
         
-       // addNote(schema);
-       // addCustomerOrder(schema);
-
+        addUsuario(schema);
         new DaoGenerator().generateAll(schema, "../DaoVao/src-gen");
     }
+	
+	 private static void addUsuario(Schema schema) {
+	        Entity note = schema.addEntity("Usuario");
+	        note.addIdProperty();
+	        note.addStringProperty("text").notNull();
+	        note.addStringProperty("nombre");
+	        note.addDateProperty("otrapropiedad");
+	    }
+
 
 }
