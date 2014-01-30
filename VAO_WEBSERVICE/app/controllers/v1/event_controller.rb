69c9fc7d9f6d0c4343dd533ce9e68c3afb1e038b
giuliomv <giulio@giulio-mv.(none)>
2014-01-29 19:40:04 -0500

    apis for create and get events

--- VAO_WEBSERVICE/app/controllers/v1/event_controller.rb
+++ VAO_WEBSERVICE/app/controllers/v1/event_controller.rb
@@ -1,2 +1,18 @@
 class V1::EventController < ApplicationController
+  
+
+  def create 
+    event = Event.create(params[:event])
+  end    
+
+  def index
+    update_time = params[:update_time]
+    events = Event.where("created_at >= ? or update_at >= ? ", update_time)
+  end
+
+  def show
+    event_id = params[:event_id]
+    event = Event.find(event_id)
+  end  
+
 end
