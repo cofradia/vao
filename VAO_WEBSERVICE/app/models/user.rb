288bcfac5ab28c6fbed9d94fa214ec80c9c79e90
giuliomv <giulio@giulio-mv.(none)>
2014-01-29 21:42:23 -0500

    devise

--- VAO_WEBSERVICE/app/models/user.rb
+++ VAO_WEBSERVICE/app/models/user.rb
@@ -3,9 +3,11 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  before_save :ensure_authentication_token
 end
