e6a397b4f052c33d078e6e07c47f31284628c9e1
Leila Lp <ly4ster@gmail.com>
2014-02-01 13:33:15 -0500

    event import

--- VAO/src/com/cofradia/vao/MainActivity.java
+++ VAO/src/com/cofradia/vao/MainActivity.java
@@ -1,6 +1,6 @@
 package com.cofradia.vao;
 
-import events.EventDetails;
+import com.cofradia.vao.events.EventDetail;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
