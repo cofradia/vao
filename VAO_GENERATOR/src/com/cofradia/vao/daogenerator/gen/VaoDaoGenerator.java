baed7d30a164db8c7fdacd1349ef869b27d9f2ce
Leila Lp <ly4ster@gmail.com>
2014-02-08 16:05:05 -0500

    event scheme added

--- VAO_GENERATOR/src/com/cofradia/vao/daogenerator/gen/VaoDaoGenerator.java
+++ VAO_GENERATOR/src/com/cofradia/vao/daogenerator/gen/VaoDaoGenerator.java
@@ -6,20 +6,41 @@ import de.greenrobot.daogenerator.Schema;
 
 public class VaoDaoGenerator {
 	public static void main(String[] args) throws Exception {
-        Schema schema = new Schema(1, "de.greenrobot.daovao");
-        
-        //Add elements to schema
-        addUsuario(schema);
-        new DaoGenerator().generateAll(schema, "../VAO/src-gen");
-    }
-	
-	 private static void addUsuario(Schema schema) {
-	        Entity usuario = schema.addEntity("Usuario");
-	        usuario.addIdProperty();
-	        usuario.addStringProperty("nombre").notNull();
-	        usuario.addStringProperty("password");
-	        usuario.addStringProperty("fbToken");
-	    }
+		Schema schema = new Schema(1, "de.greenrobot.daovao");
+		Schema event = new Schema(1, "de.greenrobot.daovao.event");
 
+		// Add elements to schema
+		addUsuario(schema);
+		addEvent(event);
+
+		DaoGenerator DaoGen = new DaoGenerator();
+		DaoGen.generateAll(schema, "../VAO/src-gen");
+		DaoGen.generateAll(event, "../VAO/src-gen");
+
+	}
+
+	private static void addUsuario(Schema schema) {
+		Entity usuario = schema.addEntity("Usuario");
+		usuario.addIdProperty();
+		usuario.addStringProperty("nombre").notNull();
+		usuario.addStringProperty("password");
+		usuario.addStringProperty("fbToken");
+	}
+
+	private static void addEvent(Schema schema) {
+		Entity event = schema.addEntity("Evento");
+		event.addIdProperty();
+		event.addStringProperty("nombre").notNull();
+		event.addStringProperty("descripcion");
+		event.addIntProperty("likes");
+		event.addFloatProperty("rating");
+		event.addStringProperty("mood");
+		event.addStringProperty("imagen");
+		event.addStringProperty("fb_evento_id").notNull();
+		event.addStringProperty("id_categoria").notNull();
+		event.addStringProperty("id_lugar").notNull();
+		event.addStringProperty("id_privacidad").notNull();
+
+	}
 
 }
