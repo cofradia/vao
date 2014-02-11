05920dbd10b79bfab9c62136e9aa5fa0dba008f7
Leila Lp <ly4ster@gmail.com>
2014-02-10 20:12:10 -0500

    event greendao changes

--- VAO/src/com/cofradia/vao/events/EventDetail.java
+++ VAO/src/com/cofradia/vao/events/EventDetail.java
@@ -1,5 +1,6 @@
 package com.cofradia.vao.events;
 
+import DataAccess.DAEvent;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
@@ -8,7 +9,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.Switch;
-
+import android.widget.TextView;
 import com.cofradia.vao.R;
 
 public class EventDetail extends Activity {
@@ -16,12 +17,28 @@ public class EventDetail extends Activity {
 	final Context context = this;
 	private Switch swtEventConfirm;
 	private CheckBox chkEventFacebookAttendance;
-	public Event event=new Event();
+	public DAEvent event;
 	
+	public EventDetail() {
+		super();
+	}
+	
+	public EventDetail(int idEvent) {
+		super();
+		this.event =new DAEvent(idEvent);		
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_event_details);
+		
+		//set comment data
+		TextView txtEventComment = (TextView) findViewById(R.id.txtEventComment);
+        txtEventComment.setText("Este es un comentario super hardcodeado xD!");
+		
+		
+		
 
 		swtEventConfirm = (Switch) findViewById(R.id.swtEvent);
 
@@ -51,21 +68,17 @@ public class EventDetail extends Activity {
 		chkEventFacebookAttendance = (CheckBox) findViewById(R.id.chkEventFacebookAttendance);
 
 		// Add a checkbox listener
-/*		chkEventFacebookAttendance
-				.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-
-					@Override
-					public void onCheckedChanged(CompoundButton buttonView,
-							boolean isChecked) {
-						// TODO Auto-generated method stub
-						if (isChecked) {
-							Toast.makeText(context, "fb!", Toast.LENGTH_LONG)
-									.show();
-						} else {
-
-						}
-					}
-				});*/
+		/*
+		 * chkEventFacebookAttendance .setOnCheckedChangeListener(new
+		 * OnCheckedChangeListener() {
+		 * 
+		 * @Override public void onCheckedChanged(CompoundButton buttonView,
+		 * boolean isChecked) { // TODO Auto-generated method stub if
+		 * (isChecked) { Toast.makeText(context, "fb!", Toast.LENGTH_LONG)
+		 * .show(); } else {
+		 * 
+		 * } } });
+		 */
 	}
 
 	@Override
@@ -74,5 +87,27 @@ public class EventDetail extends Activity {
 		getMenuInflater().inflate(R.menu.event_details, menu);
 		return true;
 	}
+
+	public Switch getSwtEventConfirm() {
+		return swtEventConfirm;
+	}
+
+	public void setSwtEventConfirm(Switch swtEventConfirm) {
+		this.swtEventConfirm = swtEventConfirm;
+	}
+
+	public CheckBox getChkEventFacebookAttendance() {
+		return chkEventFacebookAttendance;
+	}
+
+	public void setChkEventFacebookAttendance(CheckBox chkEventFacebookAttendance) {
+		this.chkEventFacebookAttendance = chkEventFacebookAttendance;
+	}
+
+	public Context getContext() {
+		return context;
+	}
+	
+	
 
 }
