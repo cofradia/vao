d36bc81b42fb18919c5df1625723354173f373fa
unknown <giuliolk@gmail.com>
2014-03-09 17:58:33 -0500

    verify logged in user

--- VAO/src/com/cofradia/vao/MainActivity.java
+++ VAO/src/com/cofradia/vao/MainActivity.java
@@ -71,7 +71,7 @@ public class MainActivity extends Activity {
         mPreferences = getSharedPreferences("CurrentUser", MODE_PRIVATE);
         fbSession = fbApi.setupFBSession(savedInstanceState, this);
         // Check whether user is already logged in and redirects to next View
-        boolean userLoggedIn = false;//user_logged_in(savedInstanceState);
+        boolean userLoggedIn = user_logged_in(savedInstanceState);
         if (!userLoggedIn) {
         	setContentView(R.layout.main);
         	setViewListeners();
