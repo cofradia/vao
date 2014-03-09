19ce93ae0e51aa90c3361ac5cfde56e36558dfb7
giomv <giuliomv@giuliomv-HP-Pavilion-dv5-Notebook-PC.(none)>
2014-03-09 17:01:12 -0500

    web service for events creation

--- VAO_WEBSERVICE/app/controllers/v1/event_controller.rb
+++ VAO_WEBSERVICE/app/controllers/v1/event_controller.rb
@@ -1,17 +1,44 @@
 class V1::EventController < ApplicationController
+	skip_before_filter :verify_authenticity_token,
+                     :if => Proc.new { |c| c.request.format == 'application/json' }
+
+  respond_to :json
 
   def create
-	data = params["event"]
-	place_obj = Place.new(:latitude => data["place_latitude"], :longitude => data["place_longitude"], :name => data["event_place_name"])
-	if place_obj.save
-		event_obj = Event.new(:name => data["event_name"], :description => data["event_description"], :category_id => data["event_category"], :place_id => place_obj.id)
-		if event_obj.save
-			date_by_event = EventDate.new(:event_id => event_obj.id, :start_date => data["event_start_date"], :end_date => data["event_end_date"], :start_time => data["event_start_time"], :end_time => data["event_end_time"])
-			if date_by_event.save
-				
+  	p "asdasda"
+		event_data = params["event"]
+		user = User.find_by_authentication_token(params["auth_token"])
+		if !user.nil?
+			place_obj = Place.new(:latitude => event_data["event_place_latitude"], :longitude => event_data["event_place_longitude"], :name => event_data["event_place_name"])
+			if place_obj.save!
+				puts place_obj.inspect
+				event_obj = Event.new(:name => event_data["event_name"], 
+															:description => event_data["event_description"], 
+															:id_category => event_data["event_category"], 
+															:place_id => place_obj.id, 
+															:user_id => user.id)
+				if event_obj.save!
+					date_by_event = EventDate.new(:event_id => event_obj.id, 
+																				:start_date => event_data["event_start_date"], 
+																				:end_date => event_data["event_end_date"])
+					if date_by_event.save!
+						render :json => {:success => true,:event_obj => event_obj}
+			    else
+	  		    render :status => :unprocessable_entity,:json => { :success => false, :info => @message.errors,
+						:data => {} }
+			    end			
+				else
+					render :status => :unprocessable_entity,:json => { :success => false, :info => @message.errors,
+						:data => {} }
+				end
+			else
+				render :status => :unprocessable_entity,:json => { :success => false, :info => @message.errors,
+						:data => {} }
 			end
+		else
+			render :status => :unprocessable_entity,:json => { :success => false, :info => "User not logged in",
+						:data => {} }
 		end
-	end
   end
 
   def index
