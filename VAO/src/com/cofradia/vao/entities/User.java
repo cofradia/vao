33e103e825aa09b59d86b622ed59299e8571a6a6
unknown <giuliolk@gmail.com>
2014-05-18 17:49:25 -0500

    user getters y setters

--- VAO/src/com/cofradia/vao/entities/User.java
+++ VAO/src/com/cofradia/vao/entities/User.java
@@ -6,16 +6,52 @@ import com.orm.SugarRecord;
 
 public class User extends SugarRecord<User>{
 
-	Long id;
-	String name;
-	String lastname;
-	String mail;
-	String fbToken;
+	private Long id;
+
+	private String name;
+	private String lastname;
+	private String mail;
+	private String fbToken;
     private String vaoToken;
     private Boolean activeSession;
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getLastname() {
+		return lastname;
+	}
+
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+
+	public String getMail() {
+		return mail;
+	}
+
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}	
+    
+	public User(Context context, String name, String lastname, String mail) {
+		super(context);
+		this.name = name;
+		this.lastname = lastname;
+		this.mail = mail;
+		
+	}
 	
-
-
 	public User(Context context) {
 		super(context);
 	}
