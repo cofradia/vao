1eee95b0601286c80ed3ac22b7ca6eb688a79339
Leila Lazo <leila.lazo@tektonlabs.com>
2014-05-18 19:09:01 -0500

    mensaje

--- VAO/src/com/cofradia/vao/entities/Place.java
+++ VAO/src/com/cofradia/vao/entities/Place.java
@@ -14,6 +14,15 @@ public class Place extends SugarRecord<Place> {
 		return id;
 	}
 
+	public Place(Context arg0, Long id, String name, Double latitude,
+			Double longitude) {
+		super(arg0);
+		this.id = id;
+		this.name = name;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
